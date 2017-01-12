0d1938627426d33c197c8725e2425c98ffc0f23a
shogunfighter <rene.cabrera.jr@gmail.com>
2017-01-12 12:15:32 +0800

    test-commit via phpstorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+import { RouterModule }         from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './components/dashboard.component';
+import { HeroDetailComponent }  from './components/hero-details.component';
+import { HeroesComponent }      from './components/heroes.component';
+import { HeroService }          from "./services/hero.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      { path: 'dashboard',  component: DashboardComponent },
+      { path: 'heroes',     component: HeroesComponent }
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
